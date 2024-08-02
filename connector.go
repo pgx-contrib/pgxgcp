c16c72d1aa21b49e2b9298e4af38be76703c1c0f
Svetlin Ralchev <iamralch@users.noreply.github.com>
2024-08-02 16:26:56 +0400

    Add the SkipAuth argument
    When the flag is set to true, we should not do any authentication at
    all. It's handy when we want to turn off the connector.
    
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

--- connector.go
+++ connector.go
@@ -10,12 +10,20 @@ import (
 
 // Connector connects to a Cloud SQL instance using the Cloud SQL Proxy.
 type Connector struct {
+	// Options to configure the GCP session
 	Options []cloudsqlconn.Option
+	// SkipAuth skips the authentication
+	SkipAuth bool
 }
 
 // BeforeConnect is called before a new connection is made. It is passed a copy of the underlying pgx.ConnConfig and
 // will not impact any existing open connections.
 func (x *Connector) BeforeConnect(ctx context.Context, conn *pgx.ConnConfig) error {
+	// skip any authentication
+	if x.SkipAuth {
+		return nil
+	}
+
 	dialer, err := cloudsqlconn.NewDialer(ctx, x.Options...)
 	if err != nil {
 		return err
